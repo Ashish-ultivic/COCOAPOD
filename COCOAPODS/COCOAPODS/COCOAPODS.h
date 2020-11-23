051f9712d73505fd0af5a5cb65a209eed807528d
Vinay <vinay.ultivic@.com>
2020-11-23 11:56:04 +0530

    Finished Framework

---
+++ COCOAPODS/COCOAPODS/COCOAPODS.h
@@ -0,0 +1,18 @@
+//
+//  COCOAPODS.h
+//  COCOAPODS
+//
+//  Created by Rakshak on 23/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for COCOAPODS.
+FOUNDATION_EXPORT double COCOAPODSVersionNumber;
+
+//! Project version string for COCOAPODS.
+FOUNDATION_EXPORT const unsigned char COCOAPODSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <COCOAPODS/PublicHeader.h>
+
+
---
+++ COCOAPODS/COCOAPODS/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  COCOAPODS
+//
+//  Created by Rakshak on 23/11/20.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "HOOOOO GYAAAAAA"
+    }
+}
